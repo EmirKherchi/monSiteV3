ec7cc147c811cc468ea5dd6824f7aed4ca0cf501
emir <contact@ekherchi.fr>
2020-07-31 09:39:11 +0200

    new style / new section function

--- index.js
+++ index.js
@@ -5,22 +5,51 @@ const landingPage = document.getElementById("LandingPage");
 
 const worksLink = document.getElementById("works");
 const contactLink = document.getElementById("contact");
+const getSections = document.querySelector('section')
+
+const addSection = (sectionName)=>{
+
+  getSections.remove();
+  const newSection = document.createElement('section');
+  const newDiv = document.createElement('div');
+  newDiv.innerHTML="<h1>Test</h1>";
+  newDiv.classList.add('marginAuto');
+  newSection.id = sectionName;
+  document.body.appendChild(newSection)
+  newSection.appendChild(newDiv);
+  newSection.classList.add("pageBase","zoomIn");
+  
+
+}
+
+
+
+
+
 
 window.addEventListener("load", function () {
   setTimeout(function () {
-    welcome.classList.add("slideUp");
+    welcome.classList.add("ZoomOut");
     return setTimeout(function () {
       logo.classList.add("opac");
       return setTimeout(function () {
-        document.body.removeChild(landingPage);
+        worksLink.classList.add("animli");
         // faire apparaître le contenu de la page d'accueil
         return setTimeout(function () {
-          worksLink.classList.add("animli");
+          contactLink.classList.add("animli");
           return setTimeout(function () {
-            contactLink.classList.add("animli");
-          }, 400);
-        }, 300);
-      }, 1000);
+            // document.body.removeChild(landingPage);
+              return setTimeout(function(){
+              logo.classList.remove('opac');
+              return setTimeout(function(){
+                logo.textContent="< emk />"
+                logo.classList.add("opac");
+                addSection("home")
+              },300)
+            },3000)
+          }, 100);
+        }, 400);
+      }, 300);
     }, 1000);
   }, 3000);
 });
