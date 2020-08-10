2728a569b653449922d5db83ba1e16abdc029fb3
emir <contact@ekherchi.fr>
2020-08-10 14:32:09 +0200

    mouseenter mouseleave ok
    check console error removechild

--- app.js
+++ app.js
@@ -68,6 +68,8 @@ const renderWorks = () => {
   } else {
     addSection("work");
     const workMainDiv = document.querySelector(".mainDiv");
+    const allimg = [];
+    const allProjectDiv = [];
 
     //fetch
 
@@ -91,26 +93,41 @@ const renderWorks = () => {
           }
           render() {
             let titleJob = document.createElement("h2");
-            let imageJob = document.createElement("img");
             let descriptionJob = document.createElement("p");
             let linkJob = document.createElement("a");
             linkJob.target = "_blank";
             linkJob.innerHTML = "Voir le projet";
+
+            let imageJob = document.createElement("img");
+
             imageJob.alt =
               "Image représentant le projet de emir développeur web front end";
 
-            titleJob.innerHTML = this.title;
             imageJob.src = this.image;
+            titleJob.innerHTML = this.title;
+
             descriptionJob.innerHTML = this.description;
             linkJob.href = this.link;
 
             const projectDiv = document.createElement("div");
             projectDiv.classList.add("projectDiv");
             workMainDiv.appendChild(projectDiv);
+            projectDiv.appendChild(imageJob);
 
+            allimg.push(imageJob);
+            allProjectDiv.push(document.querySelector(".projectDiv"));
             let elementsJob = [titleJob, imageJob, descriptionJob, linkJob];
-            elementsJob.forEach((element) => {
-              projectDiv.appendChild(element);
+            let elementsToRemove = [titleJob,descriptionJob,linkJob]
+
+            imageJob.addEventListener("mouseenter", function () {
+              elementsJob.forEach((element) => {
+                projectDiv.appendChild(element);
+              });
+              projectDiv.addEventListener('mouseleave',function(){
+                elementsToRemove.forEach((element) => {
+                  projectDiv.removeChild(element);
+                });
+              });
             });
           }
         }
